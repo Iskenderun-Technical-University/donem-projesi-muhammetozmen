69b096ccfcbc5e5a133b2243d2451f167b49d4a9
Muhammet Özmen <muh.ozmen01@gmail.com>
2023-04-28 23:39:42 +0300

    Cümleyi kağıda dökme eklendi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #█▄▀ ▄▀█ █▀█ █ ▀█▀ █░█ ▄▀█
 #█░█ █▀█ █▀▄ █ ░█░ █▀█ █▀█
 #muhammetozmen
+# -*- coding: utf-8 -*-
 
 from PIL import Image ##Çeşitli imaj etkileşimi için Pillow kütüphanesi kullandım
 import sqlite3 as sql #SQL olarak SQLite tercih ettim.
@@ -34,7 +35,7 @@ def clean_letters():
 #Bu fonksiyon ile img/workbench içinde bulunan işlenmek üzere doldurulmuş 'letter_table.png' üstündeki harfler çağrılma sırasına göre kırpılıyor  ve img/letters/alphabet klasörüne kaydediliyor
 #coords: sol üst, sağ alt kordinatları
 def crop_letter(i,letter,coords):
-    image_obj = Image.open(path_letter_table).convert("RGBA") #RGBA dönüştürme sebebimiz kırpılmış imajların etrafındaki oluşan kareyi silmek
+    image_obj = Image.open(path_letter_table)
     cropped_image = image_obj.crop(coords)
     if letter in alphabet_numbers: #Klasör kategorizasyonu
         cropped_image.save(f'./img/letters/numbers/{i}.png')
@@ -49,11 +50,11 @@ def crop_letter(i,letter,coords):
         letter_path= f'./img/letters/alphabet/{i}.png'
         import_db(letter,letter_path)
 
-
+#DEĞİŞKENLER
 i=0 #Dosya adı
 coords= [-47,51,-1,97] #0-x1,1-y1,2-x2,3-y2
 path_letter_table= './img/testdata/letter_table.png' #BURAYI DAHA SONRA WORKBENCH DİYE DÜZELT
-alphabet_all= 'ABCÇDEFGĞHİIJxKLMNOÖPRSŞTUÜxVYZ0123456789x/"\'.,?!()-+;:xabcçdefgğhiıjxklmnoöprsştuüxvyz%=><*[]{}~' #Döngü üstü harf kontrolü, x'ler alt satır temsil ediyor
+alphabet_all= 'ABCÇDEFGĞHIİJxKLMNOÖPRSŞTUÜxVYZ0123456789x/"\'.,?!()-+;:xabcçdefgğhıijxklmnoöprsştuüxvyz%=><*[]{}~' #Döngü üstü harf kontrolü, x'ler alt satır temsil ediyor
 alphabet_symbols= '/"\'.,?!()-+;:%=><*[]{}~' #Liste içindeki semboller
 alphabet_numbers= '0123456789' #Liste içindeki numaralar
 letter_path= None #Son kırpılan harfin path'ini tutar
@@ -64,6 +65,7 @@ cursor= conn.cursor()
 clean_letters()
 cursor.execute("CREATE TABLE 'image_paths' (letter,path)") #Bu tabloyu harfleri ve konumlarını saklamak ve erişmek için kullanacağız
 
+#Harf kırpma
 for letter in alphabet_all:
     if letter=='x':
         coords[0]=-47
@@ -75,3 +77,37 @@ for letter in alphabet_all:
         coords[0]+=49
         coords[2]+=49
         crop_letter(i,letter,coords)
+
+#Yazıyı oluşturma
+x_background=20 #20
+y_background= 20 #8
+user_text= str(input("Cümle gir:"))
+background= Image.open('./img/clean_temps/a4.png')
+
+for curr_letter in user_text:
+    if curr_letter==' ':
+        if x_background + 35 >=2460:
+            y_background+=40
+            x_background=20
+        else:
+            x_background+=35
+    else:
+        cursor.execute("SELECT * FROM image_paths WHERE letter=(?)",(curr_letter))
+        curr_path= cursor.fetchone()
+        #Alpha sınırlarını beyaz olarak ayarlıyoruz ve sonuna alpha kanalı oluşturuyoruz
+        foreground= Image.open(curr_path[1]).convert("RGBA")
+        foreground= foreground.crop((5,5,40,40))
+        alpha = foreground.split()[-1]
+        alpha = alpha.point(lambda x: 255 if x > 0 else 0)
+        foreground.putalpha(alpha)
+        background.paste(foreground,(x_background,y_background),foreground)
+
+    background.save('./img/workbench/text.png')
+    if x_background+35>= 2460:
+        y_background+=40
+        x_background=20
+    else:
+        x_background+=35
+    
+    #TO-DO: Satır sonuna "-" işareti eklenecek, alt satıra geçme çözülecek
+    
